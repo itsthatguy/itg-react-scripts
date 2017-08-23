f1d0767d0454b134f5dfcf42d2b892ae9dbf3d44
Kevin Altman <kevin@itsthatguy.com>
2017-08-23 13:26:36 -0400

    add dev environment to webpack

--- config/webpack/plugins.js
+++ config/webpack/plugins.js
@@ -4,9 +4,9 @@ const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 module.exports = [
   new CaseSensitivePathsPlugin(),
-  // new webpack.DefinePlugin({
-  //   CONFIG: JSON.stringify(environmentConfig)
-  // }),
+  new webpack.DefinePlugin({
+    CONFIG: JSON.stringify(process.env.NODE_ENV || 'development')
+  }),
   new webpack.optimize.OccurrenceOrderPlugin(),
   new webpack.NoEmitOnErrorsPlugin(),
 ];
