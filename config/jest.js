cdf6a7fc5422fe2a01920427f3fa3ac725b62e49
Kevin Altman <kevin@itsthatguy.com>
2017-08-24 13:22:56 -0400

    adds test scripts

---
+++ config/jest.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const paths = require('./paths');
+
+module.exports = (resolve, rootDir) => {
+  const setupTestFrameworkScriptFile =
+    fs.existsSync(paths.testsSetup)
+    && paths.testsSetup;
+
+  const defaultConfig = {
+    collectCoverageFrom: [`${paths.appSrc}/**/*.{js,jsx}`],
+    testPathIgnorePatterns: [
+      '<rootDir>[/](build|docs|node_modules|scripts)[/]',
+    ],
+    testMatch: [
+      '**/__tests__/**/?(*.)(spec|test).js?(x)',
+      '**/?(*.)(spec|test).js?(x)',
+    ],
+    testURL: 'http://localhost',
+    transform: {
+      '^.+\\.(js|jsx)$': resolve('config/jest/babelTransform.js'),
+      '^.+\\.css$': resolve('config/jest/cssTransform.js'),
+    },
+    moduleDirectories: [
+      'node_modules',
+      'src',
+      '__tests__',
+    ],
+  };
+
+  const config = Object.assign({},
+    defaultConfig,
+    rootDir && { rootDir },
+    setupTestFrameworkScriptFile && { setupTestFrameworkScriptFile }
+  );
+
+  return config;
+};
