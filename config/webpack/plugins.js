0d5841fbea1474cac8493c1087c0ecff74b4e0ad
Kevin Altman <kevin@itsthatguy.com>
2017-09-25 18:22:13 -0400

    Adds webpack plugin to copy public files to build/

--- config/webpack/plugins.js
+++ config/webpack/plugins.js
@@ -1,8 +1,10 @@
 const webpack = require('webpack');
+const { join } = require('path');
 const chalk = require('chalk');
 const InterpolateHtmlPlugin = require('interpolate-html-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const paths = require('../paths');
 const tryToLoad = require('../utils/tryToLoad');
 
@@ -13,6 +15,11 @@ const projectPlugins = tryToLoad(paths.appWebpackPlugins, []);
 
 const PLUGINS = [
   new CaseSensitivePathsPlugin(),
+  new CopyWebpackPlugin([{
+    from: join(paths.appPublic, '**/*'),
+    to: paths.appBuild,
+    ignore: [ paths.appHtml ]
+  }]),
   new InterpolateHtmlPlugin(env.raw),
   new webpack.DefinePlugin(env.stringified),
   new webpack.optimize.OccurrenceOrderPlugin(),
